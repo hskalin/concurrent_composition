6780802b281ee9432f14727bd1121f8710a18377
ootang2019 <yutang.liu2018@outlook.com>
2023-08-02 10:57:27 +0200

    ptr transfer again

--- experiment_ptr_transfer.bash
+++ experiment_ptr_transfer.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "msf ptr transfer"
-wandb agent multi-task-rl/concurrent_composition/q8x9kbyd
-
 echo "sfgpi ptr transfer"
-wandb agent multi-task-rl/concurrent_composition/gjmzhwgi
+wandb agent multi-task-rl/concurrent_composition/5km4qq2x
+
+echo "msf ptr transfer"
+wandb agent multi-task-rl/concurrent_composition/qanfxndn
 
 echo "dac ptr transfer"
-wandb agent multi-task-rl/concurrent_composition/ywxgvgjb
+wandb agent multi-task-rl/concurrent_composition/82tj1a5e
 
 echo "dacgpi ptr transfer"
-wandb agent multi-task-rl/concurrent_composition/4je7tcbg
+wandb agent multi-task-rl/concurrent_composition/p5mteo1h
 
 echo "sac ptr transfer"
-wandb agent multi-task-rl/concurrent_composition/9z76w1fg
+wandb agent multi-task-rl/concurrent_composition/332kc6vi
